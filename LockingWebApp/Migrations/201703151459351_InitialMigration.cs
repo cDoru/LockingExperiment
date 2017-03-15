fecd572bfed887aa16de83051871f49491f9f89e
Doru Cioclea <cioclea.doru@gmail.com>
2017-03-15 16:59:51 +0200

    added migrations

---
+++ LockingWebApp/Migrations/201703151459351_InitialMigration.cs
@@ -0,0 +1,27 @@
+namespace LockingWebApp.Locks.Db
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ApplicationLock",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        UtcTimestamp = c.DateTime(nullable: false),
+                        LockName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ApplicationLock");
+        }
+    }
+}
